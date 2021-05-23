9d659ef087a742e8e37626d1bbd92ee423496569
maksim-maslov <brat.maksim@gmail.com>
2021-05-23 23:51:36 +0300

    Внёс правки в модуль hw04-generics: CustomerReverseOrder.java, CustomerService.java

--- hw04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,20 +1,22 @@
 package homework;
 
 
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-public class CustomerReverseOrder extends LinkedList {
+public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
 
+    private final Deque<Customer> stackCustomer = new ArrayDeque<>();
+
     public void add(Customer customer) {
-        this.addLast(customer);
+        this.stackCustomer.addLast(customer);
     }
 
     public Customer take() {
 //        return null; // это "заглушка, чтобы скомилировать"
-        return (Customer) this.removeLast();
+        return this.stackCustomer.removeLast();
     }
 }
--- hw04-generics/src/main/java/homework/CustomerService.java
+++ hw04-generics/src/main/java/homework/CustomerService.java
@@ -1,28 +1,32 @@
 package homework;
 
+import java.util.Comparator;
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.TreeMap;
 
-public class CustomerService extends TreeMap {
+public class CustomerService {
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
 
+    private final NavigableMap<Customer, String> customers = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
 //        return null; // это "заглушка, чтобы скомилировать"
 
-        return firstEntry();
+        return this.customers.firstEntry();
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
 //        return null; // это "заглушка, чтобы скомилировать"
 
-        return higherEntry(customer);
+        return this.customers.higherEntry(customer);
     }
 
     public void add(Customer customer, String data) {
-        this.put(customer, data);
+        this.customers.put(customer, data);
     }
 }
