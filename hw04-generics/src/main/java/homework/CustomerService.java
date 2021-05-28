ec1b34718c627abd6a40aa144b64a57d5c7d09ed
maksim-maslov <brat.maksim@gmail.com>
2021-05-29 01:17:05 +0300

    Убрал изменения в сеттерах Customer.java, переписал методы CustomerService.java чтобы возвращали копии.

--- hw04-generics/src/main/java/homework/CustomerService.java
+++ hw04-generics/src/main/java/homework/CustomerService.java
@@ -1,9 +1,12 @@
 package homework;
 
+import javafx.util.Pair;
+
 import java.util.Comparator;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class CustomerService {
 
@@ -17,13 +20,17 @@ public class CustomerService {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
 //        return null; // это "заглушка, чтобы скомилировать"
 
-        return this.customers.firstEntry();
+        return this.customers.firstEntry() == null
+            ? null
+            : Map.entry(new Customer(this.customers.firstEntry().getKey().getId(), this.customers.firstEntry().getKey().getName(), this.customers.firstEntry().getKey().getScores()), this.customers.firstEntry().getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
 //        return null; // это "заглушка, чтобы скомилировать"
 
-        return this.customers.higherEntry(customer);
+        return this.customers.higherEntry(customer) == null
+            ? null
+            : Map.entry(new Customer(this.customers.higherEntry(customer).getKey().getId(), this.customers.higherEntry(customer).getKey().getName(), this.customers.higherEntry(customer).getKey().getScores()), this.customers.higherEntry(customer).getValue());
     }
 
     public void add(Customer customer, String data) {
